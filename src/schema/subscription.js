37f89d93184f9ee93af88be4e905e75f7b768094
mwmando <mando24wael@gmail.com>
2019-03-13 17:44:35 +0200

    add new file search

--- src/schema/subscription.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-subscription = new mongoose.Schema({
-    email:{
-        type: String
-    }
-});
-module.exports = mongoose.model("Followers",subscription);
