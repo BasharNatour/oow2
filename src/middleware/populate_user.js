05ee8a31e563cdb23c5afcf45f5e80be520933a5
Bashar <you@example.com>
2019-04-01 23:26:06 +0200

    change

--- src/middleware/populate_user.js
+++ src/middleware/populate_user.js
@@ -3,7 +3,7 @@ const User =require("../schema/user");
 
 module.exports = function populate_user(req,res,next){
     if(req.session.user_id){
-        User.findById(req.session.user_id).then((doc)=>{
+        User.findById(req.session.user_id).populate({ path: 'governorate', populate: { path: 'country' }}).then((doc)=>{
             req.user = doc;
             next();
         }).catch(next)
--- src/router/client/dashboard/editSetting.js
+++ src/router/client/dashboard/editSetting.js
@@ -2,7 +2,19 @@ const express = require("express");
 const router = new express.Router();
 module.exports = router;
 const populate_user = require("../../../middleware/populate_user");
+const Country =require("../../../schema/country");
+const Governorate= require("../../../schema/governorates");
 
-router.get("/",populate_user,(req,res)=>{
-    res.render("edit-setting-user");
+router.get("/",populate_user,(req,res,next)=>{
+    const user = req.user;
+    console.log("=======================================");
+    console.log("Hey Hey Hey");
+    console.log("========================================");
+    console.log(user);
+    Country.find({}).then((countries)=>{
+    Governorate.find({}).then((governorates)=>{
+
+        res.render("edit-setting-user",{user,countries,governorates});
+    }).catch(next);
+    }).catch(next);
 });
