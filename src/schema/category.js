352400e642b1ffc03c0ec866f0d3946656fc0624
WinterCore <hogobbl@gmail.com>
2019-04-22 22:29:18 +0200

    Added postremove hook to delete the deleted category's image & background

--- src/schema/category.js
+++ src/schema/category.js
@@ -1,5 +1,7 @@
-const mongoose =require("mongoose");
-
+const mongoose = require("mongoose");
+const path     = require("path");
+const fs       = require("fs");
+const util     = require("util");
 
 let categories = new mongoose.Schema({
     name:{
@@ -17,6 +19,15 @@ let categories = new mongoose.Schema({
     longdescription:{
         type:String
     },
+});
+
+categories.post("remove", { query : true, document : true }, function (next) {
+    Promise.all([
+        util.promisify(fs.unlink)(path.join(process.cwd(), "public", this.background)),
+        util.promisify(fs.unlink)(path.join(process.cwd(), "public", this.image))
+    ])
+    .then(next)
+    .catch(next);
 });
 
 module.exports = mongoose.model("categories",categories);
