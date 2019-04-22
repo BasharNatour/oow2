10ff49a8fdb041f6e594145902f3560dbec5be4f
WinterCore <hogobbl@gmail.com>
2019-04-22 22:28:50 +0200

    Added presave hook to hash the password

--- src/schema/admin.js
+++ src/schema/admin.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt  = require("bcrypt");
 
 let admin = new mongoose.Schema({
     name:{
@@ -10,9 +11,17 @@ let admin = new mongoose.Schema({
     password:{
         type:String
     }
-    
-
 });
 
+admin.pre("save",function(next){
+    if(this.isNew){
+        bcrypt.hash(this.password, 10).then((hash) => {
+            this.password = hash;
+            next();
+        }).catch(console.log);
+    } else {
+        next();
+    }
+});
 module.exports = mongoose.model("admin",admin);
 
