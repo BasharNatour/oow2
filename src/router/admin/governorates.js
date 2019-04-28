9f5a61efa77c2fe5eb9abd37c9d82155c760665d
WinterCore <hogobbl@gmail.com>
2019-04-28 11:10:19 +0200

    Added governorates endpoints

---
+++ src/router/admin/governorates.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const multer = require("multer");
+const router  = new express.Router();
+module.exports = router;
+
+const Governorate = require("../../schema/governorates");
+
+router.post("/", (req, res, next) => {
+    const governorate = new Governorate(req.body);
+    console.log(governorate);
+    governorate
+        .save()
+        .then(() => res.json({ data : governorate }))
+        .catch(next);
+});
+
+router.put("/:id", (req, res, next) => {
+    const data = req.body;
+    Governorate
+        .findById(req.params.id)
+        .then(doc => {
+            doc.name = data.name;
+            doc
+                .save()
+                .then(() => res.json({ data : doc }))
+                .catch(next);
+        })
+        .catch(next);
+});
+
+
+router.delete("/:id", (req, res, next) => {
+    Governorate
+        .findById(req.params.id)
+        .then((doc) => doc.remove())
+        .then(() => res.sendStatus(200))
+        .catch(next);
+});
--- src/router/admin/index.js
+++ src/router/admin/index.js
@@ -8,3 +8,4 @@ router.use(require("cors")());
 router.use(require("./auth"));
 router.use("/categories", require("./categories"));
 router.use("/countries", require("./countries"));
+router.use("/governorates", require("./governorates"));
